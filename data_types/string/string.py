1874115890d3262898a17dcf2fca809bcd6d1abe
ujwal32 <uzl.shrestha@gmail.com>
2025-05-11 21:04:11 +0545

    python

--- data_types/string/string.py
+++ data_types/string/string.py
@@ -1,36 +1,47 @@
 # str – Text: "Hello" always denoted inside ""
 
-str1="hello"
-str2="world"
-result=str1 + str2
-print(result)
+# str1="hello"
+# str2="world"
+# result=str1 + str2
+# print(result)
 
-repeat="hello"*3
-print(repeat)
+# repeat="hello"*3
+# print(repeat)
 
-print(str1[0])
-print(str1[-1])
+# print(str1[0])
+# print(str1[-1])
 
-text= "hello world"
-print(text[0:5])
+# text= "hello world"
+# print(text[0:5])
 
-print(text[:5])
+# print(text[:5])
 
-print(text[:6])
+# print(text[:6])
 
-print(text[::2])
+# print(text[::2])
 
-print(text[::-1])
+# print(text[::-1])
 
-txt="python" is text
-print("python" is text)
+# txt="python" is text
+# print("python" is text)
 
-print(text.upper())
-print(text.lower())
-print(text.title())
-print(text.capitalize())
+# print(text.upper())
+# print(text.lower())
+# print(text.title())
+# print(text.capitalize())
 
-name="       python             "
-print(name.strip())
-print(name.lstrip())
-print(name.rstrip())
+# name="       python             "
+# print(name.strip())
+# print(name.lstrip())
+# print(name.rstrip())
+
+# txt="i love india"
+# txt1=txt.replace("india","nepal")
+# print(txt1)
+ 
+txt="a,b,c"
+t=txt.split(",")
+print(t)
+
+joined_text="-".join(txt)
+print(joined_text)
