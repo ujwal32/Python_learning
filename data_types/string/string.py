ab8c03b25d4f84cea45bcc40f81c212b23a0e82b
ujwal32 <uzl.shrestha@gmail.com>
2025-05-11 20:41:52 +0545

    python

--- data_types/string/string.py
+++ data_types/string/string.py
@@ -11,7 +11,7 @@ print(repeat)
 print(str1[0])
 print(str1[-1])
 
-text="hello world"
+text= "hello world"
 print(text[0:5])
 
 print(text[:5])
@@ -21,3 +21,11 @@ print(text[:6])
 print(text[::2])
 
 print(text[::-1])
+
+txt="python" is text
+print("python" is text)
+
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.capitalize())
