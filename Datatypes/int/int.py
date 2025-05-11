c1697d515a0e7a3c8451502c995b2b6d3185d7bc
ujwal32 <uzl.shrestha@gmail.com>
2025-05-11 17:05:01 +0545

    python

--- Datatypes/int/int.py
+++ Datatypes/int/int.py
@@ -1,4 +1,4 @@
-#int = number withput decimal points
+#int = number without decimal points and it takes negative points too
 
 
 
@@ -9,5 +9,19 @@ num2 =500
 num3=10000
 
 sum= num1+num2+num3   
+dif= num2-num1
+prod=num1*num2
+quotient=num1//num2
+modulus=num2%num1
+power=num1**num2
 
+print(dif)
 print(sum)
+print(prod)
+print(quotient)
+print(modulus)
+print(power)
+
+quo=12//3  #basically it takes remainder
+
+print(quo)
