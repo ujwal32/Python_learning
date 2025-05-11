85324e25ce2c4c2f173f38ea4a8efdcb8e03cdbe
ujwal32 <uzl.shrestha@gmail.com>
2025-05-11 17:19:32 +0545

    python

---
+++ Datatypes/float/float.py
@@ -0,0 +1,19 @@
+# float=same as integer but in decimal
+
+num1=10.5
+num2 =500.2
+num3=10000.1
+
+sum= num1+num2+num3   
+dif= num2-num1
+prod=num1*num2
+quotient=num1//num2
+modulus=num2%num1
+# power=num1**num2
+
+print(dif)
+print(sum)
+print(prod)
+print(quotient)
+print(modulus)
+# print(power)
--- ujwal_test1.py
+++
@@ -1,5 +0,0 @@
-"""
-
-ujwal shrestha 
-
-"""
