8173e7730d64434610d563a9fffda976af91e7a1
ujwal32 <uzl.shrestha@gmail.com>
2025-05-15 20:41:35 +0545

    python

--- loop/while_loop/while.py
+++ loop/while_loop/while.py
@@ -1,4 +1,14 @@
-count=1
-while count<=5:
+# count=1
+# while count<=5:
+#     print(count)
+#     count+=1
+
+# count=15
+# while count > = 10:
+#     print(count)
+#     count-=1
+
+count = 15
+while count >= 10:
     print(count)
-    count+=1
+    count -= 1
