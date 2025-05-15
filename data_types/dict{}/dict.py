e95b16b326a042bb45b48bffe4743ba382f48886
ujwal32 <uzl.shrestha@gmail.com>
2025-05-15 18:28:56 +0545

    python

--- data_types/dict{}/dict.py
+++ data_types/dict{}/dict.py
@@ -1,5 +1,7 @@
 # dict=unordered, unchabgeable, key value pairs(keys and values)
 
-student={"name":"ujwal","age":"32","country":"nepal"}
-print(student)
-print(student["name"])
+student={"student1":{"name":"ujwal","age":"32","country":"nepal"},
+
+"student2":{"name":"dipehs","age":"34","country":"ktm"}}
+
+print(student["student1"]["country"])
