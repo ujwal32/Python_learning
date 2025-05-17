e112bcec72db85e14ca0f8f7037c43caca6ac64e
ujwal32 <uzl.shrestha@gmail.com>
2025-05-17 22:36:16 +0545

    python

--- loop/for_loop/for.py
+++ loop/for_loop/for.py
@@ -3,6 +3,6 @@ for i in range (1,5):
         print(f"i={i},j={j}")
 
 
-# for i in range (10,15): #11,12,13,14 #outer loop
-#     for j in range(20,25): #20,21,22,23,24 #inner loop
-#         print(f"i={i},j={j}")
+for i in range (10,15): #11,12,13,14 #outer loop
+    for j in range(20,25): #20,21,22,23,24 #inner loop
+        print(f"i={i},j={j}")
